b44f3d68b86c3cd59f82e07670c1690b96af10f6
mhaf4krr <muftihyderalifarooqi786@gmail.com>
2018-04-11 23:38:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+loadJokes();
+
+
+document.querySelector('#button-click').addEventListener('click',loadJokes);
+let output=document.querySelector('.result-div');
+
+function loadJokes(){
+   console.log('event heard');
+   const xhr=new XMLHttpRequest();
+
+   xhr.open('GET',`http://api.icndb.com/jokes/random`,true);
+    
+   xhr.onprogress=function(){
+
+      output.innerHTML=`Loading Something New`;
+   }
+   
+
+
+   xhr.onload=function(){
+      if(this.status===200){
+      
+      
+      let response=JSON.parse(this.responseText);
+      output.innerHTML=`${response.value.joke}`; 
+      }
+   }
+
+   xhr.send();
+}
+
+onmousemove = function(e){
+document.body.style.backgroundColor= `rgb(225, ${e.clientX}, ${e.clientY})`;
+}
+
